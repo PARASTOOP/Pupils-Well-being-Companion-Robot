bcf919d535de9ec0809608189666330768dfc986
Swallow <57176066+PARASTOOP@users.noreply.github.com>
2021-06-09 23:12:56 +0100

    Add files via upload

---
+++ Cobot_dewey/Dewey_Introduction/translations/translation_en_US.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Face_recognision/behavior.xar:/Say</name>
+        <message>
+            <location filename="Face_recognision/behavior.xar" line="0"/>
+            <source>Hello Bob</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>Face_recognision/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="Face_recognision/behavior.xar" line="0"/>
+            <source>Hello David</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>Face_recognision/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="Face_recognision/behavior.xar" line="0"/>
+            <source>Hello Simon</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>Face_recognision/behavior.xar:/Say (3)</name>
+        <message>
+            <location filename="Face_recognision/behavior.xar" line="0"/>
+            <source>Hello Mike</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>Face_recognision/behavior.xar:/Say (4)</name>
+        <message>
+            <location filename="Face_recognision/behavior.xar" line="0"/>
+            <source>Hello Hana</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>Face_recognision/behavior.xar:/Say (5)</name>
+        <message>
+            <location filename="Face_recognision/behavior.xar" line="0"/>
+            <source>Hello Sara</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>greeting/behavior.xar:/Say</name>
+        <message>
+            <location filename="greeting/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+</TS>
---
+++ Cobot_dewey/Telling_jokes/telling.Joke.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="get_mood" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="hey" src="hey.mp3" />
+        <File name="applause1" src="applause1.wav" />
+        <File name="Clapping" src="Clapping.mp3" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
